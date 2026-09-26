7128bb169cc3148a91bfb91f34bf70590583e4e7
2Spartiate <2Spartiate@gmail.com>
2026-09-26 11:41:46 +0200

    tv: remove obsolete search primary-focus marker

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -541,7 +541,6 @@ const Header: React.FC = () => {
                 <form onSubmit={handleSearchSubmit} className="relative">
                   <input
                     ref={searchInputRef}
-                    data-tv-primary-focus="search"
                     type="text"
                     value={headerQuery}
                     onChange={handleQueryChange}
